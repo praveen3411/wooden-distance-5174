ab77e55e11931f7a989f8de990e9db911deaa527
praveen3411 <ypraveen341@gmail.com>
2022-08-27 00:06:47 +0530

    day-3 project

--- myglamm-website/src/Components/Mainpages/Makeup.jsx
+++ myglamm-website/src/Components/Mainpages/Makeup.jsx
@@ -1,14 +1,19 @@
+import AddData from "../add";
 import FirstNavbar from "../Navbars/FirstNavbar";
 import ThirdNavbar from "../Navbars/NavbarThird";
 import SecondNavbar from "../Navbars/SecondNavbar";
 
-export default function Makeup(){
-    return(
-        <div>
-            <FirstNavbar/>
-            <SecondNavbar/>
-            <ThirdNavbar/>
-            <h2>jfsldkfjskljflksdj</h2>
-        </div>
-    )
+export default function Makeup() {
+  return (
+    <div>
+      <FirstNavbar />
+      <SecondNavbar />
+      <ThirdNavbar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <AddData />
+    </div>
+  );
 }
